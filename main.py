44d1a19b32ba1ea8d0982d7a9a9800888a8a1fea
kglspl <kglspl@spletoholik.si>
2024-01-20 21:30:42 +0100

    Add sidebar for navigation along the axes

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ class Scroll3DViewer:
     scrolldata_loaded = None  # chunk of data which is loaded into memory
     canvas_display_matrix = None  # transformation on this data to display it on canvas (contains rotations and translations)
 
+    _canvas_3d_photoimgs = None
+
     CANVAS_PAD = 150  # padding of the cube when displayed on canvas
     SCROLLDATA_CACHE_PAD = math.ceil(math.sqrt(3 * CANVAS_PAD**2))  # performance optimization - calculate in advance padding needed when loading scrolldata chunk
     SHIFT_TO_SCROLLDATA_LOADED_CENTER = np.array(
@@ -41,6 +43,8 @@ class Scroll3DViewer:
         self.canvas_display_matrix = np.identity(4)
         self.init_ui()
         self.load_scroll_data_around_position(*initial_position_yxz)
+        self.update_canvas()
+        self.update_nav3d_display()
 
     def parse_args(self):
         argparser = argparse.ArgumentParser(usage="%(prog)s [OPTION]...", description="3D viewer for Vesuvius Challenge scroll data.")
@@ -53,7 +57,23 @@ class Scroll3DViewer:
         # self.root.attributes("-zoomed", True)
         self.root.title("Scroll 3D Viewer")
 
-        self.canvas = tk.Canvas(self.root, bg="white")
+        self.center_frame = tk.Frame(self.root, bg="white")
+        self.center_frame.pack(side=tk.TOP, fill="both", expand=True)
+
+        self.nav3d_frame = tk.Frame(self.center_frame, width="201", bg="white")
+        self.nav3d_frame.pack(side=tk.LEFT, fill=tk.Y, expand=False)
+
+        self.canvas_z = tk.Canvas(self.nav3d_frame, bg="white", highlightthickness=5, highlightbackground="green")
+        self.canvas_z.pack(fill="both", expand=True)
+        self.canvas_x = tk.Canvas(self.nav3d_frame, bg="white", highlightthickness=5, highlightbackground="red")
+        self.canvas_x.pack(fill="both", expand=True)
+        self.canvas_y = tk.Canvas(self.nav3d_frame, bg="white", highlightthickness=5, highlightbackground="blue")
+        self.canvas_y.pack(fill="both", expand=True)
+        self.canvas_z_text = self.canvas_z.create_text(10, 10, anchor=tk.NW, text="Z: /", fill="red", font=("Helvetica", 15, "bold"))
+        self.canvas_x_text = self.canvas_x.create_text(10, 10, anchor=tk.NW, text="X: /", fill="red", font=("Helvetica", 15, "bold"))
+        self.canvas_y_text = self.canvas_y.create_text(10, 10, anchor=tk.NW, text="Y: /", fill="red", font=("Helvetica", 15, "bold"))
+
+        self.canvas = tk.Canvas(self.center_frame, bg="white")
         self.canvas.pack(fill="both", expand=True)
 
         self.canvas.bind("<ButtonPress-1>", self.on_canvas_drag_start)
@@ -72,10 +92,10 @@ class Scroll3DViewer:
             # - find the offset of the new center (which might be dislocated in 3D, not just 2D) using self.canvas_display_matrix
             # - load new scroll data chunk around previous center + offset
             # - change self.canvas_display_matrix so that it only contains rotation (no translations)
-            offset = self.canvas_display_matrix @ np.array([0, 0, 0, 1])
-            new_position_yxz = [self.position_yxz[i] + round(offset[i]) for i in range(3)]
-            self.load_scroll_data_around_position(*new_position_yxz)
+            y, x, z = self.get_current_position()
+            self.load_scroll_data_around_position(y, x, z)
             self.update_canvas()
+            self.update_nav3d_display()
             return
 
         # A/S/D - rotate in different directions
@@ -83,7 +103,13 @@ class Scroll3DViewer:
             axis = ["a", "s", "d"].index(ev.keysym)
             self.rotate90(axis)
             self.update_canvas()
+            self.update_nav3d_display()
             return
+
+    def get_current_position(self):
+        offset = self.canvas_display_matrix @ np.array([0, 0, 0, 1])
+        new_position_yxz = (self.position_yxz[i] + round(offset[i]) for i in range(3))
+        return new_position_yxz
 
     def rotate90(self, axis):
         R = np.identity(4)
@@ -124,6 +150,7 @@ class Scroll3DViewer:
     def on_canvas_drag_move(self, event):
         self.navigation.on_drag_move(event)
         self.update_canvas()
+        self.update_nav3d_display()
 
     def on_canvas_drag_end(self, event):
         # now that the drag is over, roll up its transformation matrix into our display matrix:
@@ -163,13 +190,68 @@ class Scroll3DViewer:
         self.canvas.create_line((pw - 10, ph), (pw + 10, ph), width=1, fill="red")
         self.canvas.create_line((pw, ph - 10), (pw, ph + 10), width=1, fill="red")
 
+    def update_nav3d_display(self):
+        scroll_y, scroll_x, scroll_z = self.get_current_position()
+
+        imgs = []
+        for i, c in enumerate([self.canvas_z, self.canvas_x, self.canvas_y]):
+            c.update()  # this is needed to get the real w/h of canvases
+            pw, ph = c.winfo_width() // 2, c.winfo_height() // 2
+            if i == 0:
+                img_data = (self.scrolldata.dset[scroll_y - ph : scroll_y + ph, scroll_x - pw : scroll_x + pw, scroll_z] // 256).astype(np.uint8)
+            elif i == 1:
+                img_data = (self.scrolldata.dset[scroll_y - ph : scroll_y + ph, scroll_x, scroll_z - pw : scroll_z + pw] // 256).astype(np.uint8)
+            else:
+                img_data = (self.scrolldata.dset[scroll_y, scroll_x - ph : scroll_x + ph, scroll_z - pw : scroll_z + pw] // 256).astype(np.uint8)
+            img = Image.fromarray(img_data).convert("RGBA")
+            imgs.append(img)
+
+        # Draw vicinity points on images while still in Image format:
+        # imgs = self.draw_vicinity_points_on_nav3d(imgs, pw, ph, surface_x, surface_y, scroll_x, scroll_y, scroll_z)
+        # imgs = self.draw_normals_on_nav3d(imgs, pw, ph, scroll_nx, scroll_ny, scroll_nz)
+
+        # Render photoimages on nav3d canvases:
+        self._canvas_3d_photoimgs = []  # PhotoImage's must be saved on instance or they will be garbage collected before displayed
+        for i, c in enumerate([self.canvas_z, self.canvas_x, self.canvas_y]):
+            self._canvas_3d_photoimgs.append(ImageTk.PhotoImage(image=imgs[i]))  # must be on instance or it will be garbage collected before it is displayed
+            c.create_image(5, 5, anchor=tk.NW, image=self._canvas_3d_photoimgs[i])
+
+        # Draw center navigation lines on canvases:
+        b = 5  # border offset
+        for i, c in enumerate([self.canvas_z, self.canvas_x, self.canvas_y]):
+            if i == 0:  # 0 == z
+                c.create_line((pw + b, 0), (pw + b, ph // 2), width=2, fill="red")
+                c.create_line((pw + b, round(1.5 * ph)), (pw + b, 2 * ph + 1), width=2, fill="red")
+                c.create_line((0, ph + b), (pw // 2, ph + b), width=2, fill="blue")
+                c.create_line((round(1.5 * pw), ph + b), (2 * pw + 1, ph + b), width=2, fill="blue")
+            elif i == 1:  # 1 == x
+                c.create_line((pw + b, 0), (pw + b, ph // 2), width=2, fill="green")
+                c.create_line((pw + b, round(1.5 * ph)), (pw + b, 2 * ph + 1), width=2, fill="green")
+                c.create_line((0, ph + b), (pw // 2, ph + b), width=2, fill="blue")
+                c.create_line((round(1.5 * pw), ph + b), (2 * pw + 1, ph + b), width=2, fill="blue")
+            else:  # 2 == y
+                c.create_line((pw + b, 0), (pw + b, ph // 2), width=2, fill="green")
+                c.create_line((pw + b, round(1.5 * ph)), (pw + b, 2 * ph + 1), width=2, fill="green")
+                c.create_line((0, ph + b), (pw // 2, ph + b), width=2, fill="red")
+                c.create_line((round(1.5 * pw), ph + b), (2 * pw + 1, ph + b), width=2, fill="red")
+
+        # Update labels with 3D coordinates:
+        self.canvas_z.itemconfigure(self.canvas_z_text, text=f"Z: {scroll_z}")
+        self.canvas_x.itemconfigure(self.canvas_x_text, text=f"X: {scroll_x}")
+        self.canvas_y.itemconfigure(self.canvas_y_text, text=f"Y: {scroll_y}")
+        self.canvas_z.tag_raise(self.canvas_z_text)
+        self.canvas_x.tag_raise(self.canvas_x_text)
+        self.canvas_y.tag_raise(self.canvas_y_text)
+        self.canvas_z.pack(fill=tk.BOTH, expand=True)
+        self.canvas_x.pack(fill=tk.BOTH, expand=True)
+        self.canvas_y.pack(fill=tk.BOTH, expand=True)
+
     def on_exit(self):
         print("Closing scroll data.")
         self.scrolldata.close()
 
     def run(self):
         try:
-            self.update_canvas()
             self.root.mainloop()
         finally:
             self.on_exit()
